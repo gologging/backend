444e6e053d8b6498f60c327b410a13d98ce8f459
TidosDK <git@msnn.dev>
2024-08-27 19:32:17 +0200

    Removed commented code

--- src/views.py
+++ src/views.py
@@ -6,5 +6,3 @@ views = Blueprint('views', __name__)
 @views.route('/healthz', methods=['GET'])
 def index():
     return "<body style='background: black;'><p style='color: white;'>Healthy!</p></body>"
-    # return json.dumps({'name': 'alice',
-                    #    'email': 'alice@outlook.com'})
